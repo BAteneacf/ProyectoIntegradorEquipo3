c07ae0de6e76ce94a2903e8f5ab425a0bbbccda1
Areli <areli.moreno213@gmail.com>
2023-11-07 18:16:48 -0600

    Se corrigio la ortografía del cuestionario de contacto

--- js/main.js
+++ js/main.js
@@ -90,7 +90,7 @@ const inputs = document.querySelectorAll("#formulario input");
 const expresiones = {
 	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
 	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-	telefono: /^\d{7,10}$/ // 7 a 10 numeros.
+	telefono: /^\d{10}$/ // 7 a 10 numeros.
 	
 }
 
