f174ad749c81682f5a0f06085b01c7473b3fba87
Areli <areli.moreno213@gmail.com>
2023-11-10 07:30:05 -0600

    intento de arreglo del formulario de contacto

--- js/contacto.js
+++ js/contacto.js
@@ -6,7 +6,7 @@ const inputs = document.querySelectorAll("#formulario input");
 const expresiones = {
 	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
 	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-	telefono: /^\d{10}$/ // 10 numeros.
+	telefono: /^\(?(\d{3})\)?[-]?(\d{3})[-]?(\d{4})$/ // 10 numeros.
 	
 }
 
@@ -17,65 +17,68 @@ const campos = {
 	
 }
 
+
+
 const validarFormulario = (e) => {
 	switch (e.target.name) {
 		case "nombre":
-			validarCampo(expresiones.nombre, e.target, 'nombre');
+			validarCampo(expresiones.nombre, e.target, "nombre");
 		break;
 
 		case "correo":
-			validarCampo(expresiones.correo, e.target, 'correo');
+			validarCampo(expresiones.correo, e.target, "correo");
 		break;
 
 		case "telefono":
-			validarCampo(expresiones.telefono, e.target, 'telefono');
+			validarCampo(expresiones.telefono, e.target, "telefono");
 		break;
 	}
 }
 
 const validarCampo = (expresion, input, campo) => {
 	if(expresion.test(input.value)){
-		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
-		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
-		document.querySelector(`#grupo__${campo} i`).classList.add('fa-check-circle');
-		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-times-circle');
-		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
+		document.getElementById("grupo__${campo}").classList.remove("formulario__grupo-incorrecto");
+		document.getElementById("grupo__${campo}").classList.add("formulario__grupo-correcto");
+		document.querySelector("#grupo__${campo} i").classList.add("fa-check-circle");
+		document.querySelector("#grupo__${campo} i").classList.remove("fa-times-circle");
+		document.querySelector("#grupo__${campo} .formulario__input-error").classList.remove("formulario__input-error-activo");
 		campos[campo] = true;
 	} else {
-		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
-		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
-		document.querySelector(`#grupo__${campo} i`).classList.add('fa-times-circle');
-		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-check-circle');
-		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
+		document.getElementById("grupo__${campo}").classList.add("formulario__grupo-incorrecto");
+		document.getElementById("grupo__${campo}").classList.remove("formulario__grupo-correcto");
+		document.querySelector("#grupo__${campo} i").classList.add("fa-times-circle");
+		document.querySelector("#grupo__${campo} i").classList.remove("fa-check-circle");
+		document.querySelector("#grupo__${campo} .formulario__input-error").classList.add("formulario__input-error-activo");
 		campos[campo] = false;
 	}
 }
 
 
 inputs.forEach((input) => {
-	input.addEventListener('keyup', validarFormulario);
-	input.addEventListener('blur', validarFormulario);
+	input.addEventListener("keyup", validarFormulario);
+	input.addEventListener("blur", validarFormulario);
 });
 
 
-const btn = document.getElementById('button');
 
-document.getElementById('formulario')
- .addEventListener('submit', function(event) {
+const btn = document.getElementById("button");
+
+document.getElementById("formulario")
+ .addEventListener("submit", function(event) {
    event.preventDefault();
 
-   btn.value = 'Enviando...';
+   btn.value = "Enviando...";
 
    console.log(this);
-   const serviceID = 'service_983v66l';
-   const templateID = 'template_tmy2qf5';
+   const serviceID = "service_983v66l";
+   const templateID = "template_tmy2qf5";
 
    emailjs.sendForm(serviceID, templateID, this)
     .then(() => {
-      btn.value = 'ENVIAR';
-      alert('Mensaje enviado correctamente');
+      btn.value = "ENVIAR";
+      alert("Mensaje enviado correctamente");
     }, (err) => {
-      btn.value = 'ENVIAR';
+      btn.value = "ENVIAR";
       alert(JSON.stringify(err));
     });
 }); 
