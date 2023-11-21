ed7a35299fc5867d1db792088386ab3ebb868941
JessOV <oliveravitej@gmail.com>
2023-11-21 01:44:24 -0600

    Se agrega diseño a página de asesores

--- js/asesores.js
+++ js/asesores.js
@@ -5,15 +5,22 @@ function modalFormat (info) {
         <div class="modal-dialog modal-lg modal-dialog-centered">
           <div class="modal-content">
             <div class="modal-header">
-              <h1 class="modal-title fs-5" id="exampleModalLabel">${info.name}</h1>
+              <h1 class="modal-title-detalles fs-5" id="exampleModalLabel"><strong> MÁS DETALLES </strong></h1>
               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class "modal-body">
-            <img src="${info.img}" style="heigth:250px; width:250px">
-              ${info.description}
+            <div class="modal-nombre">
+              <h1 class="modal-name fs-5" id="exampleModalLabel">${info.name}</h1>
+            </div>
+            <div class= "modal-body">
+            <img id="imagenesAsesores" src="${info.img}" style="heigth:250px; width:250px; display: flex; align-items: center;">
+              
+            <p id="descripcionAsesor"> ${info.description} </p>
+            </div>
+            <div class="modal-precio">
+              <h4 class="modal-price fs-5" id="exampleModalLabel">${info.precio}</h4>
             </div>
             <div class="modal-footer">
-              <button type="button" onclick="agregarACarrito(${info.id}, '${info.name}', '${info.img}')" class="btn btn-secondary" data-bs-dismiss="modal">Agregar asesoría</button>
+              <button type="button" onclick="agregarACarrito(${info.id}, '${info.name}', '${info.img}')" class="btn2 btn-secondary" data-bs-dismiss="modal">Agregar asesoría</button>
             </div>
           </div>
         </div>
@@ -26,8 +33,8 @@ function addItem(item) {
           <img src="${item.img}" class="card-img-top" alt="image">
           <div class="card-body">
               <h5 class="card-title" style="text-align: center">${item.name}</h5>
-              <p class="card-text" style="text-align: center">${item.description}</p>
-              <a href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal_${item.id}" style="margin-bottom:1rem; padding:25px; border:200px;">Ver más</a>
+              <p class="card-text" style="text-align: center">${item.habilidad}</p>
+              <a href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal_${item.id}" style="margin-bottom:1rem; padding:8px; margin:10px 100px; border:100px; display:flex; justify-content: center">Ver más</a>
           </div>
       </div>
       <br/>`;
--- js/listaAsesores.js
+++ js/listaAsesores.js
@@ -2,100 +2,97 @@ let listaAsesores = [
     {
       'name': 'Lizeth Bravo Acevedo',
       'img': './src/Lizy.png',
-      'description': 'HTML5 y CSS3',
+      'habilidad': 'HTML5, Git y CSS3',
       'id': 1,  // Debe coincidir con el "id" del modal
-      'description': 'Contenido del modal 1'
+      'description': 'Habilidad principal: HTML5. <br/> Habilidades adicionales: Git y CSS3. <br/><br/> ¡Hola! Soy Lizeth, desarrolladora Java Full Stack con pasión por tecnología y gestión de proyectos. He perfeccionado la gestión de proyectos y desarrollo web en los últimos años. Me emociona compartir mis conocimientos en Git, HTML5 y CSS3.',
+      'precio': '$150.00 / hora'
     },
     {
       'name': 'Ana Cristina Hernández',
       'img': './src/Ana_Cris.png',
-      'description': 'HTML5 y CSS3',
+      'habilidad': 'JavaScript y Bootstrap',
       'id': 2,
-      'description': 'Contenido del modal 2'
+      'description': 'Habilidad principal: JavaScript. <br/> Habilidad adicional: Bootstrap. <br/><br/> ¡Hola! Soy Ana Cristina Hernández, desarrolladora web con pasión por la tecnología. Mi experiencia en investigación me brinda habilidades avanzadas de análisis y comunicación. Espero con ansias poder trabajar contigo.',
+      'precio': '$150.00 / hora'
     },
     {
       'name': 'Jessica L. Olivera',
       'img': './src/lucero.png',
-      'description': 'HTML5, CSS3 y Java',
+      'habilidad': 'Java, Trello y MySQL',
       'id': 3,
-      'description': 'Contenido del modal 3',
+      'description': 'Habilidad principal: JAVA. <br/> Habilidades adicionales: Trello y MySQL. <br/><br/> ¡Hola!Soy Jessica L. Olivera, una especialista en Java Full Stack con pasión por enseñar y perfeccionar habilidades técnicas y blandas en programación. Me apasiona aceptar desafíos y aprender constantemente. Y espero que pueda transmitirte todos mis conocimientos y energía',
+      'precio': '$150.00 / hora'
     },
     {
       'name': 'Alma G. Martínez',
       'img': './src/Alma.png',
-      'description': 'CSS3, y Trello',
+      'habilidad': 'CSS3, y Metodología SCRUM',
       'id': 4,
-      'description': 'Contenido del modal 4'
+      'description': 'Habilidad principal: CCS3. <br/> Habilidad adicional: Metodología SCRUM. <br/><br/> ¡Hola! Soy Alma, Desarrolladora Full Stack apasionada por la tecnología y el aprendizaje. He perfeccionado mis habilidades para transmitirlas de manera sencilla. Mi verdadera pasión es el conocimiento y compartirlo con otros.',
+      'precio': '$150.00 / hora'
     },
     {
       'name': 'Amalia Elizalde',
       'img': './src/lia.png',
-      'description': 'Bootstrap, Trello y Java',
+      'habilidad': 'JAVA, Trello y Bootstrap',
       'id': 5,
-      'description': 'Contenido del modal 5'
+      'description': 'Habilidad principal: JAVA. <br/> Habilidades adicionales: Trello y Bootstrap. <br/><br/> ¡Hola! Soy Amalia Elizalde, desarrolladora Java Full Stack apasionada por la programación. He perfeccionado mis habilidades en desarrollo web durante el último año. Más allá de los números y logros, mi pasión es aprender y mantenerme actualizada en TI.',
+      'precio': '$150.00 / hora'
     },
     {
       'name': 'Areli Moreno',
       'img': './src/Areli.png',
-      'description': 'Javascript, CSS3 y Figma',
+      'habilidad': 'Javascript, CSS3 y Figma',
       'id': 6,
-      'description': 'Contenido del modal 6'
+      'description': 'Habilidad principal: JavaScript. <br/> Habilidad adicional: CSS3 y Figma. <br/><br/> ¡Hola! Soy Areli Moreno, desarrolladora web apasionada por las TI. He perfeccionado mis habilidades técnicas y blandas. Mi verdadera pasión es compartir conocimientos y ayudar a otros a alcanzar sus metas.',
+      'precio': '$150.00 / hora'
     },
     {
       'name': 'Abril Fernández',
       'img': './src/Abril.png',
-      'description': 'HTML5 y Java',
+      'habilidad': 'HTML5, Java y GitHub',
       'id': 7,
-      'description': 'Contenido del modal 7',
+      'description': 'Habilidad principal: HTML5. <br/> Habilidad adicional: JAVA y Github. <br/><br/> ¡Hola! Soy Abril, latinoamericanista y desarrolladora web apasionada por la enseñanza y el área TI. A lo largo de años, he perfeccionado la docencia y el desarrollo web. Mi pasión es compartir conocimiento y ayudarte a alcanzar tu éxito profesional.',
+      'precio': '$150.00 / hora'
     },
     {
       'name': 'Almendra Zamudio',
       'img': './src/Almendra.png',
-      'description': 'HTML5 y CSS3',
+      'habilidad': 'Bootstrap y Figma',
       'id': 8,
-      'description': 'Contenido del modal 8'
+      'description': 'Habilidad principal: HTML5. <br/> Habilidad adicional: CSS3. <br/><br/> ¡Hola!',
+      'precio': '$150.00 / hora'
     },
     {
       'name': 'Rosa Pérez',
       'img': './src/Rosa.png',
-      'description': 'HTML5 y CSS3',
+      'habilidad': 'JavaScript',
       'id': 9,
-      'description': 'Contenido del modal 9'
+      'description': 'Habilidad principal: HTML5. <br/> Habilidad adicional: CSS3. <br/><br/> ¡Hola!',
+      'precio': '$150.00 / hora'
     },
     {
       'name': 'Jazmín Corona',
       'img': './src/Jaz.jpg',
-      'description': 'HTML5 y CSS3',
+      'habilidad': 'Java',
       'id': 10,
-      'description': 'Contenido del modal 10'
+      'description': 'Habilidad principal: HTML5. <br/> Habilidad adicional: CSS3. <br/><br/> ¡Hola!',
+      'precio': '$150.00 / hora'
     },
-    {
-      'name': 'Rosa Pérez',
-      'img': './src/Rosa.png',
-      'description': 'HTML5 y CSS3',
-      'id': 11,
-      'description': 'Contenido del modal 11'
-    },
-    {
-      'name': 'Jazmín Corona',
-      'img': './src/Jaz.jpg',
-      'description': 'HTML5 y CSS3',
-      'id': 12,
-      'description': 'Contenido del modal 12'
-    },
-    {
-        'name': 'Lizeth Bravo Acevedo',
-        'img': './src/Lizy.png',
-        'description': 'HTML5 y CSS3',
-        'id': 13,  // Debe coincidir con el "id" del modal
-        'description': 'Contenido del modal 1'
-      },
-      {
-        'name': 'Ana Cristina Hernández',
-        'img': './src/Ana_Cris.png',
-        'description': 'HTML5 y CSS3',
-        'id': 14,
-        'description': 'Contenido del modal 2'
-      }
+    // Se van a agregar 2 asesores más... NO ELIMINAR
+    // { 
+    //   'name': 'Rosa Pérez',
+    //   'img': './src/Rosa.png',
+    //   'description': 'HTML5 y CSS3',
+    //   'id': 11,
+    //   'description': 'Contenido del modal 11'
+    // },
+    // {
+    //   'name': 'Jazmín Corona',
+    //   'img': './src/Jaz.jpg',
+    //   'description': 'HTML5 y CSS3',
+    //   'id': 12,
+    //   'description': 'Contenido del modal 12'
+    // },
   ];
   
